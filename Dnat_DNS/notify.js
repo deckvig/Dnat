86c52f837cf166cc0e94289382aebc50f8ca1f7c
deckvig <2008.zhengxing@gmail.com>
2016-06-05 13:04:17 +0800

    add notify

---
+++ Dnat_DNS/notify.js
@@ -0,0 +1,16 @@
+$.notify({
+    title: 'Note:',
+    message: 'If you are first time to this web,please visit <strong>FAQ</strong> page for help!',
+    url: 'https://www.dnat.site/FAQ',
+    target: '_self',
+}, {
+    placement: {
+        from: "top",
+        align: "center"
+    },
+    offset: {
+        x: 10,
+        y: 100
+    },
+
+});
