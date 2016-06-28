925ad66cd9d11d20672ccb738c3cfd0a3236e3ab
deckvig <2008.zhengxing@gmail.com>
2016-06-28 23:31:55 +0800

    update notify

--- Dnat_DNS/notify.js
+++ Dnat_DNS/notify.js
@@ -1,6 +1,6 @@
 $.notify({
     title: 'Note:',
-    message: 'If you are first time to this website,please visit <strong>FAQ</strong> page for help!',
+    message: '添加 Aplle support DNS，加速 Aplle 各项服务访问速度。点击右上角<strong> FAQ </strong>访问使用教程',
     url: 'https://www.dnat.site/FAQ',
     target: '_self',
 }, {
